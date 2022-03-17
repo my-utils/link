193adc3a6e2d0a8d34375a072f4fd6b55d5f51a1
wangliuyin <wangliuyin@jtexpress.com>
2022-03-17 17:11:13 +0800

    save

--- src/create.js
+++ src/create.js
@@ -1,6 +1,11 @@
 
 const exec = require('child_process').exec
 const inquirer = require('inquirer')
+const path = require('path')
+const del = require('del')
+const ora = require('ora');
+
+const mySpinner = ora('获取远程数据中')
 
 function create(gitList) {
   inquirer
@@ -49,24 +54,23 @@ function create(gitList) {
       console.log('error :>> ', error)
     })
 
-
-
   function clone(gitUrl, branch) {
     function clear(fn) {
-      exec(`rm -rf src/jms-common`, function (error) {
-        if (!error) {
-          console.log('清除完成')
-          fn()
-        } else {
-          console.log('清除目录异常', error)
-        }
-      })
+      try {
+        const delinfo = del.sync(path.join(process.cwd(), 'src/jms-common'))
+        console.log('清除完成', delinfo)
+        fn()
+      } catch (error) {
+        console.log('error', error)
+      }
     }
 
     clear(function () {
       const shell = `git clone -b ${branch} ${gitUrl} src/jms-common`
       console.log('克隆中 :>> ', shell)
+      mySpinner.start()
       exec(shell, function (error) {
+        mySpinner.stop()
         if (!error) {
           console.log('克隆完成')
         } else {
